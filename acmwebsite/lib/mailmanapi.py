36776f7f8ff1e856035d9633095963695cf81447
Jack Rosenthal <jack@rosenth.al>
2017-08-05 13:15:50 -0600

    bring in the mailman api

---
+++ acmwebsite/lib/mailmanapi.py
@@ -0,0 +1,89 @@
+# Title: Mailman List Admin API
+# Author: Jack Rosenthal
+# Date: 2016-02-21
+import concurrent.futures
+import requests
+import re
+import string
+
+class ListAdminAPI:
+    def __init__(self, admin_url, admin_auth):
+        self.admin_url = admin_url
+        r = requests.post(admin_url, data={"adminpw": admin_auth})
+        if not r.ok:
+            raise RuntimeError("Error authenticating to mailinglist")
+        self.cookies = r.cookies
+
+    def member_options_url(self, email):
+        return self.admin_url.replace("/admin/", "/options/") + "/" + email
+
+    def bulk_subscribe(self, *emails, invite=False, welcome=True, notify_owner=True, message=''):
+        post_data = {
+                "subscribe_or_invite": int(invite),
+                "send_welcome_msg_to_this_batch": int(welcome),
+                "send_notifications_to_list_owner": int(notify_owner),
+                "subscribees": '\n'.join(emails) + '\n',
+                "invitation": message + '\n'
+            }
+        r = requests.post(self.admin_url + "/members/add", cookies=self.cookies, data=post_data)
+        if not r.ok:
+            raise RuntimeError("Error subscribing members")
+
+    def bulk_unsubscribe(self, *emails, notify_user=True, notify_owner=True):
+        post_data = {
+                "send_unsub_ack_to_this_batch": int(notify_user),
+                "send_unsub_notifications_to_list_owner": int(notify_owner),
+                "unsubscribees": '\n'.join(emails) + '\n'
+            }
+        r = requests.post(self.admin_url + "/members/remove", cookies=self.cookies, data=post_data)
+        if not r.ok:
+            raise RuntimeError("Error unsubscribing members")
+
+    def get_member_options(self, email):
+        r = requests.get(self.member_options_url(email), cookies=self.cookies)
+        if not r.ok:
+            raise RuntimeError("Unable to get member options")
+
+        p = re.compile(r'<input type=radio name="([^"]*)" value="([^"]*)" CHECKED>')
+        opts = {}
+        for m in p.finditer(r.text):
+            opts[m.group(1)] = int(m.group(2))
+
+        name_regex = re.compile(r'name="fullname" size="[^"]*" value="([^"]*)"')
+        m = name_regex.search(r.text)
+        if m:
+            opts["fullname"] = m.group(1)
+        return opts
+
+    def set_member_options(self, email, fullname='', **kwargs):
+        def check_response(future):
+            if not future.result().ok:
+                raise RuntimeError("Unable to set options on mailman")
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            if kwargs:
+                opts = {}
+                for k, v in kwargs.items():
+                    opts[k] = int(v)
+                opts['options-submit'] = 1
+                options_f = executor.submit(requests.post, self.member_options_url(email), cookies=self.cookies, data=opts)
+                options_f.add_done_callback(check_response)
+            if fullname:
+                opts = {'change-of-address': 1, 'fullname': fullname}
+                fullname_f = executor.submit(requests.post, self.member_options_url(email), cookies=self.cookies, data=opts)
+                fullname_f.add_done_callback(check_response)
+
+    def mymail_subscribe(self, mines_username, fullname='', message=''):
+        if not mines_username or set(mines_username) - set(string.ascii_letters + string.digits + '._'):
+            raise ValueError('Invalid input for Mines username')
+        r = requests.get("http://toilers.mines.edu/toilers-cgi-bin/namequery.cgi?un=" + mines_username)
+        if r.ok and r.text == '?\n':
+            raise ValueError('Bad Mines username')
+        if r.ok and not fullname:
+            fullname = r.text.strip()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            mines_f = executor.submit(self.bulk_subscribe, mines_username + '@mines.edu', message=message)
+            mymail_f = executor.submit(self.bulk_subscribe, mines_username + '@mymail.mines.edu', notify_owner=False, welcome=False)
+            mines_f.add_done_callback(lambda f:self.set_member_options(mines_username + '@mines.edu', fullname=fullname))
+            mymail_f.add_done_callback(lambda f:self.set_member_options(mines_username + '@mymail.mines.edu', fullname=fullname + " (alias for posting)", disablemail=True, conceal=1))
+
