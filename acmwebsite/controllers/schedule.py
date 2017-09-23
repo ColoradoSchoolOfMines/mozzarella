cf3379ebbfbdba9b7f1b1053fd79e82c7fcc1c1d
Sumner Evans <sumner.evans98@gmail.com>
2017-09-23 14:52:06 -0600

    #14 fix issue with the schedule page

--- acmwebsite/controllers/schedule.py
+++ acmwebsite/controllers/schedule.py
@@ -10,26 +10,31 @@ from acmwebsite.model import DBSession, Meeting
 
 class ScheduleController(BaseController):
     def __init__(self):
-        self.meetings = DBSession.query(Meeting).order_by(Meeting.date).all()
+        self.meetings = DBSession.query(Meeting).order_by(Meeting.date)
 
     @expose('acmwebsite.templates.schedule')
     @expose('json', exclude_names=['page'])
-    def _index(self):
+    def index(self):
         """Handle the schedule page."""
 
         # Allow CORS
         response.headers.add("Access-Control-Allow-Origin", "*")
-        return dict(page='schedule', meetings=self.meetings.filter(
+
+        # Filter meetings that occurred in the past
+        upcoming_meetings = self.meetings.filter(
             Meeting.date > datetime.datetime.now() - datetime.timedelta(hours=3)
-        ))
+        ).all()
+
+        return dict(page='schedule', meetings=upcoming_meetings)
 
     @expose(content_type='text/calendar')
     def acm(self):
+        """ Returns the iCal version of the ACM schedule """
         cal = Calendar()
         cal.add('prodid', '-//Mines ACM//web//EN')
         cal.add('version', '2.0')
 
-        for m in self.meetings:
+        for m in self.meetings.all():
             event = Event()
             event.add('summary', m.title)
             event.add('description', m.description)
@@ -37,6 +42,7 @@ class ScheduleController(BaseController):
             event.add('dtstart', m.date)
             event.add('dtend', m.date + datetime.timedelta(hours=2))
             event.add('dtstamp', m.date)
+
             cal.add_component(event)
 
         return cal.to_ical()
