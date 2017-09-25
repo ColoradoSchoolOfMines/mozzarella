ae139605737caa65bc5bcd2c664f4b09cab8d8a5
Jack Rosenthal <jack@rosenth.al>
2017-09-25 13:31:33 -0600

    Remove vestigial JavaScripts from Sumner's attempt to make non-standard OS controls

--- acmwebsite/public/js/schedule.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  // Copy the schedule URL to the clipboard.
-  // https://stackoverflow.com/a/30810322/2319844
-  $('#copy-link').on('click', function() {
-    $('#schedule-url').select();
-    document.execCommand('copy');
-  });
-})(jQuery);
