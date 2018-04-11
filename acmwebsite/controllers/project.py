8025ab7b3e595d2a3f047184377a79f544c6893e
David Florness <edwargix@gmail.com>
2018-04-10 19:52:20 -0600

    PEP-8 for acmwebsite/controllers/project.py

--- acmwebsite/controllers/project.py
+++ acmwebsite/controllers/project.py
@@ -2,25 +2,24 @@
 
 from tg import expose
 
-from acmwebsite.controllers import user
 from acmwebsite.lib.base import BaseController
+from acmwebsite.lib.card import Card
 from acmwebsite.model import DBSession, Project
+import acmwebsite.controllers.user as user
 
-from kajiki import XMLTemplate
-
-import acmwebsite.controllers.user
-from acmwebsite.lib.card import Card, CardTypes
 
 def project_card_gen(user):
     for proj in user.projects:
-        args = dict(project=proj)
+        args = {'project': proj}
         yield Card(args, args)
 
-acmwebsite.controllers.user.cards.register(
+
+user.cards.register(
     project_card_gen,
     title_template="project_title",
     body_template="project_body",
 )
+
 
 class ProjectsController(BaseController):
     """Controller for listing all projects"""
