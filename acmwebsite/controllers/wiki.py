c20008ef9afdbd24783214e41b12b14dfd574ab7
Brandon Verkamp <jadelclemens@gmail.com>
2018-06-10 17:57:40 -0600

    Wiki now adds .rst extension automatically, properly errors when file is not found

--- acmwebsite/controllers/wiki.py
+++ acmwebsite/controllers/wiki.py
@@ -13,14 +13,18 @@ class WikiController(BaseController):
     OC code donut steel
     """
 
-    @expose('acmwebsite.templates.wiki')
+    @expose('acmwebsite.templates.wikiview')
     def index(self, pagename="FrontPage"):
         repo = Repository(tg.config.get('wiki.repo'))
         tb = repo.TreeBuilder(repo.head.peel().tree)
         if tb.get(''.join((pagename, '.rst'))) is None:
-            pass #redirect to edit page
-        blob = repo.get(repo.index[pagename].id)
+            tg.abort(404, "Page not found")
+        blob = repo.get(repo.index[''.join((pagename, '.rst'))].id)
         return dict(page=pagename, content=blob.data)
+
+    @expose('acmwebsite.templates.wikiedit')
+    def edit(self, pagename):
+        pass
 
     def save(self, pagename):
         pass
