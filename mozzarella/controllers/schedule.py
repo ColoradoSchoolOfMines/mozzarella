71fe3e1d2e3d72169513adaf588c19b1edded4b9
Noah Fields <fields.noah.6@gmail.com>
2018-11-26 18:29:28 -0700

    replaced (almost) all instances of the term 'acmwebsite' with 'mozzarella'. So far as I can tell everything is working fine. I left a single instance untouched, in a comment.

---
+++ mozzarella/controllers/schedule.py
@@ -0,0 +1,53 @@
+"""
+Schedule page and iCalendar controller
+"""
+import datetime
+import pytz
+
+from icalendar import Calendar, Event
+from tg import expose, response, request, config
+
+from mozzarella.lib.base import BaseController
+from mozzarella.model import DBSession, Meeting
+
+
+class ScheduleController(BaseController):
+    def __init__(self):
+        self.meetings = DBSession.query(Meeting).order_by(Meeting.date)
+
+    @expose('mozzarella.templates.schedule')
+    @expose(content_type='text/calendar')
+    def index(self):
+        """
+        Handle the schedule page. If the request is for the .ics file, it will
+        return the schedule in iCal format.
+        """
+        if request.response_type == 'text/calendar':
+            return self.ical_schedule()
+
+        # Filter meetings that occurred in the past
+        upcoming_meetings = self.meetings.filter(
+            Meeting.date > datetime.datetime.now() - datetime.timedelta(hours=3)
+        ).all()
+
+        return dict(page='schedule', meetings=upcoming_meetings)
+
+    def ical_schedule(self):
+        """ Returns the iCalendar version of the schedule """
+        cal = Calendar()
+        cal.add('prodid', config.get('meetings.icalendar.prodid'))
+        cal.add('version', '2.0')
+
+        for m in self.meetings.all():
+            event = Event()
+            event.add('summary', m.title)
+            event.add('description', m.description)
+            event.add('location', m.location)
+            d = m.date.replace(tzinfo=pytz.timezone(config.get('meetings.timezone')))
+            event.add('dtstart', d)
+            event.add('dtend', d + m.duration)
+            event.add('dtstamp', d)
+
+            cal.add_component(event)
+
+        return cal.to_ical()
---
+++ mozzarella/model/mailmessage.py
@@ -0,0 +1,89 @@
+"""Models for mailing list messages in Mozzarella."""
+import tg
+import re
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.types import Unicode, DateTime, Integer
+from sqlalchemy.orm import relationship
+from depot.fields.sqlalchemy import UploadedFileField
+
+from mozzarella.model import DeclarativeBase, DBSession, User
+
+user_p = re.compile(r'<(\w+)@(?:mymail\.|alumni\.)?mines\.edu>')
+
+
+
+class MailMessage(DeclarativeBase):
+    __tablename__ = 'mailmessages'
+
+    message_id = Column(Unicode, primary_key=True)
+    body = Column(Unicode, nullable=True)
+    from_ = Column(Unicode, nullable=False)
+    date = Column(DateTime, nullable=False)
+    subject = Column(Unicode, nullable=True)
+    parent_message_id = Column(Unicode, nullable=True)
+    attachments = relationship("MailAttachment", back_populates="message")
+
+    @property
+    def url(self):
+        return tg.url('/mailinglist/message/' + self.message_id + '.html')
+
+    @property
+    def parent(self):
+        return (DBSession.query(MailMessage)
+                         .filter(MailMessage.message_id == self.parent_message_id)
+                         .one_or_none())
+
+    @property
+    def children(self):
+        return (DBSession.query(MailMessage)
+                         .filter(MailMessage.parent_message_id == self.message_id)
+                         .all())
+
+    @property
+    def linkname(self):
+        return self.from_ + ': ' + self.subject if self.subject else ''
+
+    @property
+    def next_by_date(self):
+        return (DBSession.query(MailMessage)
+                         .filter(MailMessage.date > self.date)
+                         .order_by(MailMessage.date)
+                         .first())
+
+    @property
+    def prev_by_date(self):
+        return (DBSession.query(MailMessage)
+                         .filter(MailMessage.date < self.date)
+                         .order_by(MailMessage.date.desc())
+                         .first())
+
+    @property
+    def mines_username(self):
+        m = user_p.search(self.from_)
+        return m.group(1) if m else None
+
+    @property
+    def user(self):
+        return (DBSession.query(User)
+                         .filter(User.user_name == self.mines_username)
+                         .one_or_none())
+
+    @property
+    def from_display(self):
+        u = self.user
+        if u:
+            return '<a href="{u.profile_url}">{u}</a>'.format(u=u)
+        else:
+            return self.from_.replace('@', '&nbsp;at&nbsp;').replace('<', '(').replace('>', ')')
+
+
+class MailAttachment(DeclarativeBase):
+    __tablename__ = 'mailattachments'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    message_id = Column(Unicode, ForeignKey('mailmessages.message_id'))
+    message = relationship("MailMessage", back_populates="attachments")
+    file = Column(UploadedFileField)
+
+
+__all__ = ['MailMessage', 'MailAttachment']
