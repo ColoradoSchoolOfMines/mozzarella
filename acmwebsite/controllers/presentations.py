bb15f2a9de4d49143c9a95eb927249e3e77fb341
David Florness <edwargix@gmail.com>
2019-01-06 18:12:27 -0600

    Create presentation upload form

--- acmwebsite/controllers/presentations.py
+++ acmwebsite/controllers/presentations.py
@@ -1,17 +1,42 @@
 """Presentations controller"""
-from tg import expose
+from tg import expose, validate, tmpl_context, flash, redirect
+from sprox.formbase import AddRecordForm
 
 from acmwebsite.model import DBSession
+from acmwebsite.model.presentation import Presentation
+from acmwebsite.model.auth import User
 from acmwebsite.model.presentation import Presentation
 from acmwebsite.lib.base import BaseController
 
 __all__ = ['PresentationsController']
 
+
+class NewPresentationForm(AddRecordForm):
+    __model__ = Presentation
+    __require_fields__ = ['title']
+new_presentation_form = NewPresentationForm(DBSession)
+
+
 class PresentationsController(BaseController):
     @expose('acmwebsite.templates.presentations')
-    def index():
+    def index(self):
         """List all presentations"""
         presentations = list(DBSession.query(Presentation))
         presentations.sort(key=lambda p: p.date, reverse=True)
 
         return dict(page='presentations', presentations=presentations)
+
+    @expose('acmwebsite.templates.presentation_upload')
+    def upload_form(self, **kw):
+        tmpl_context.form = new_presentation_form
+        return dict(value=kw)
+
+    @validate(new_presentation_form, error_handler=upload_form)
+    @expose()
+    def upload(self, **kw):
+        del kw['sprox_id']  # required by sprox
+        kw['authors'] = [DBSession.query(User).get(id) for id in kw['authors']]
+        pres = Presentation(**kw)
+        DBSession.add(pres)
+        flash('Your presentation was successfully uploaded')
+        redirect('/index')
