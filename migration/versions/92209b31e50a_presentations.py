da889b0d7da5a9249cbc4b11b2af242f3bff2caa
David Florness <edwargix@gmail.com>
2019-01-06 18:21:28 -0600

    Create presentation model migration script

---
+++ migration/versions/92209b31e50a_presentations.py
@@ -0,0 +1,36 @@
+"""
+Presentations
+
+Revision ID: 92209b31e50a
+Revises: 36e3bdae7eab
+Create Date: 2019-01-02 16:22:15.746372
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '92209b31e50a'
+down_revision = '36e3bdae7eab'
+
+
+def upgrade():
+    op.create_table(
+        'presentation',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.Unicode),
+        sa.Column('description', sa.String(32)),
+        sa.Column('date', sa.DateTime),
+    )
+
+    op.create_table(
+        'presentation_author',
+        sa.Column('presentation_id', sa.Integer, sa.ForeignKey('presentation.id'), primary_key=True),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('tg_user.user_id'), primary_key=True),
+    )
+
+
+def downgrade():
+    op.drop_table('presentation')
+    op.drop_table('presentation_author')
