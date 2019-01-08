c00214dc84c269337380e6acfc964a00991406fc
Sumner Evans <sumner.evans98@gmail.com>
2019-01-08 16:05:13 -0700

    Add arbitrary file uploading

--- acmwebsite/model/presentation.py
+++ acmwebsite/model/presentation.py
@@ -4,23 +4,43 @@ Presentations are slides, files, or other presentables that the club shares
 among its cohorts, usually during Meetings.
 """
 
-from sqlalchemy import Column, Table, ForeignKey
+from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import relation
-from sqlalchemy.types import Integer, Unicode, String, Date
-
-from depot.fields.sqlalchemy import UploadedFileField
-from depot.fields.specialized.image import UploadedImageWithThumb
+from sqlalchemy.types import Date, Integer, String, Unicode
 
 from acmwebsite.model import DeclarativeBase, metadata
 from acmwebsite.model.auth import User
-
+from depot.fields.specialized.image import UploadedImageWithThumb
+from depot.fields.sqlalchemy import UploadedFileField
 
 presentation_author_table = Table(
     'presentation_author', metadata,
-    Column('presentation_id', Integer,
-           ForeignKey('presentation.id'), primary_key=True),
-    Column('user_id', Integer,
-           ForeignKey('tg_user.user_id'), primary_key=True))
+    Column(
+        'presentation_id',
+        Integer,
+        ForeignKey('presentation.id'),
+        primary_key=True),
+    Column(
+        'user_id', Integer, ForeignKey('tg_user.user_id'), primary_key=True))
+
+
+class PresentationFile(DeclarativeBase):
+    """A model that defines a file which is associated with a presentation."""
+    __tablename__ = 'presentation_file'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    presentation_id = Column(Integer, ForeignKey('presentation.id'))
+    description = Column(String(32), nullable=False)
+    file = Column(UploadedFileField)
+
+    @property
+    def icon(self):
+        icons = {'application/pdf': 'fa-file-pdf-o'}
+        return icons[self.file.type]
+
+    @property
+    def url(self):
+        return file.url  # TODO
 
 
 class Presentation(DeclarativeBase):
@@ -37,22 +57,19 @@ class Presentation(DeclarativeBase):
     description = Column(Unicode)
     date = Column(Date, nullable=False)
     thumbnail = Column(UploadedFileField(upload_type=UploadedImageWithThumb))
-    authors = relation(
-        User,
-        secondary=presentation_author_table,
-        backref='presentations'
-    )
     repo_url = Column(Unicode(512))
-    slides_pdf = Column(UploadedFileField)
-    latex_source = Column(UploadedFileField)
+
+    authors = relation(
+        User, secondary=presentation_author_table, backref='presentations')
+    files = relation(PresentationFile, backref='presentation_id')
 
     @property
     def page_buttons(self):
-        return dict(
-            repo_url=(self.repo_url,
-                      'View repo', 'fa fa-github') if self.repo_url else None,
-            slides_pdf=(self.slides_pdf.url,
-                        'View Slides PDF', 'fa fa-file-pdf-o') if self.slides_pdf else None,
-            latex_source=(self.latex_source.url,
-                          'View LaTeX source', 'fa fa-file-code-o') if self.latex_source else None,
-        )
+        buttons = [
+            (self.repo_url, 'View repo',
+             'fa-github') if self.repo_url else None,
+        ]
+        for file in self.files:
+            buttons.append((file.url, file.description, file.icon))
+
+        return buttons
