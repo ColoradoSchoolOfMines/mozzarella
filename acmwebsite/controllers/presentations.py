246672ab87cd5bfff3040466953a4526be1dfae9
David Florness <edwargix@gmail.com>
2019-01-09 13:01:33 -0700

    Start widget for multiple files

--- acmwebsite/controllers/presentations.py
+++ acmwebsite/controllers/presentations.py
@@ -1,7 +1,8 @@
 """Presentations controller"""
 from tg import expose, validate, tmpl_context, flash, redirect
 from sprox.formbase import AddRecordForm
-from tw2.forms import UrlField
+import tw2.core as twc
+import tw2.forms as twf
 
 from acmwebsite.model import DBSession
 from acmwebsite.model.presentation import Presentation
@@ -12,10 +13,22 @@ from acmwebsite.lib.base import BaseController
 __all__ = ['PresentationsController']
 
 
+# WIP
+class MultiFileField(twf.widgets.FormField):
+    inline_engine_name = 'kajiki'
+    template = '''
+    <div id="${w.id}">
+        <input type="file" />
+        <input type="submit" value="Add file" />
+    </div>
+    '''
+
+
 class NewPresentationForm(AddRecordForm):
     __model__ = Presentation
     __require_fields__ = ['title', 'date']
-    repo_url = UrlField('repo_url')
+    repo_url = twf.UrlField('repo_url')
+    files = MultiFileField('files')
 new_presentation_form = NewPresentationForm(DBSession)
 
 
